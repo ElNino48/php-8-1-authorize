aa60afa56f6fb1133a4f5994c67ec8cf45b103cf
ElNino48 <111755259+ElNino48@users.noreply.github.com>
2024-04-26 16:37:26 +0300

    Initial commit

---
+++ layout/footer.php
@@ -0,0 +1,26 @@
+    <b><i>
+
+        <span id="hello"> 
+
+            Welcome! 
+
+            <span id="username"></span> 
+
+            <br><button id="logout">logout</button> 
+
+        </span>
+
+    </i></b>
+    
+    <br><span id="unauthorized">Welcome (unauthorized)! </span> 
+
+    <br><a href="index.php">Go to Main Page </a> 
+
+    <br><a href="page1.php">Go to Page 1 </a> 
+
+    <br><a href="page2.php">Go to Page 2 </a> 
+
+    <script src="script.js"></script> 
+
+</body> 
+</html>
